e14f813fb3f874f9bd433c4d2a592c5eaa6c3a92
cjpurackal <christeejacobs@gmail.com>
2020-09-14 22:35:41 -0700

    init

---
+++ src/bbox_tf.cpp
@@ -0,0 +1,13 @@
+#include "ignition/math/Pose3.hh"
+#include "ros/ros.h"
+#include "ignition/math/Pose3.hh"
+#include "ros/ros.h"
+
+
+std::string robot_name;
+std::string base_link_frame;
+std::string map_frame;
+std::string artifact_origin_frame;
+std::string service_name = "/subt/pose_from_artifact_origin";
+
+
